fc853f74e8dbff39b80099485d4a7caf00f16301
TheoAt <theo.atlani@gmail.com>
2024-04-18 17:01:28 +0200

    add converter section and api call

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,9 +1,73 @@
+import { useEffect, useState } from 'react'
 import './converter.css'
 
 export default function CurrencyConverter() {
-    return(
+
+    const [amount, setAmount] = useState(1)
+    const [fromCurrency, setFromCurrency] = useState('EUR')
+    const [toCurrency, setToCurrency] = useState('USD')
+    const [exchangeRate, setExchangeRate] = useState()
+    const [convertedAmount, setConvertedAmount] = useState()
+
+    async function fetchExchangeRate() {
+        const apiResponse = await fetch(`https://open.er-api.com/v6/latest/${fromCurrency}`, {
+            method: 'GET'
+        })
+
+        const result = await apiResponse.json()
+
+        const calculateRate = result?.rates[toCurrency]
+        setExchangeRate(calculateRate)
+        setConvertedAmount((amount * calculateRate).toFixed(2))
+    }
+
+    useEffect(() => {
+        fetchExchangeRate()
+    }, [fromCurrency, toCurrency, amount])
+
+    const handleAmountChange = (e) => {
+        setAmount(e.target.value)
+    }
+
+    const handleFromCurrencyChange = (e) => {
+        setFromCurrency(e.target.value)
+    }
+
+    const handleToCurrencyChange = (e) => {
+        setToCurrency(e.target.value)
+    }
+
+    return (
         <div className="converter-container">
-            Test
+            <div className="input-container">
+                <input
+                    value={amount}
+                    onChange={handleAmountChange}
+                    type="number"
+                    name='amount'
+                    placeholder='Enter Amount'
+                />
+
+                <select value={fromCurrency} onChange={handleFromCurrencyChange}>
+                    <option value={'EUR'}>EUR</option>
+                    <option value={'USD'}>USD</option>
+                    <option value={'INR'}>INR</option>
+                </select>
+            </div>
+
+            <p>To</p>
+
+            <div className="input-container">
+                <input type="text" value={convertedAmount} readOnly />
+
+                <select value={toCurrency} onChange={handleToCurrencyChange}>
+                    <option value={'USD'}>USD</option>
+                    <option value={'EUR'}>EUR</option>
+                    <option value={'INR'}>INR</option>
+                </select>
+            </div>
+
+            <p>Exchange rate : 1 {fromCurrency} = {exchangeRate} {toCurrency}</p>
         </div>
     )
 }
